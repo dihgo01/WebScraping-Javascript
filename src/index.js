2b2bde4ac02212f95abe61f0deba77f1802c73fd
dihgo01 <luka.poke9@gmail.com>
2021-10-18 14:31:57 -0300

    Add feature the title and Avaible

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless : false});
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.setViewport({ width: 1240, height: 3898 })
   await page.goto('https://www.icarros.com.br/ford');
   await page.mouse.down(800, 3800)
   await page.evaluate(() => {
-        const nodeListImg = document.querySelectorAll('.img-responsive')
-        const nodeListTitle = document.querySelectorAll('.title')
-        const nodeListRate = document.querySelectorAll('.price-results__rate')
+    const nodeListImg = document.querySelectorAll('.img-responsive')
 
-        const ListArray = [...nodeListTitle,...nodeListImg,...nodeListRate,]
+    const nodeListRate = document.querySelectorAll('.price-results__rate')
 
-        const list = ListArray.map( items => ({
-            title: items.h2,
-            src: items.src,
-            p: items.p, 
-        }))
+    const listNode = document.querySelectorAll('.title--brand__highlight')
+    let listTitle = []
+    let listAvaible = []
 
-        console.log(list) 
+    for (var i = 0; i < listNode.length; i++) {
+
+      listTitle[i] = listNode[i].textContent.replace('\n' ,'');
+      
+    }
+    for (var i = 0; i < nodeListRate.length; i++) {
+
+      listAvaible[i] = nodeListRate[i].textContent.replace('\n' ,'');
+      
+    }
+
+    const ListArray = [...nodeListImg]
+
+    const list = ListArray.map(items => ({
+      src: items.src,
+    }))
+
+    console.log(list)
+    console.log(listTitle)
+    console.log(listAvaible)
   });
 
   //await browser.close();
